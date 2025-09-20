2f958442dbf0c741716b54b2823ecbcb40f9c677
Kalzemic <Mizizov@gmail.com>
2025-09-20 12:01:15 +0300

    more mobile adjustments

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -2,22 +2,22 @@ import '../Styles/AboutPage.css'
 
 export default function AboutPage() {
     return (
-        <div className='about-page'>
 
-            <div className='about-body'>
-                <div className='about-image-container'>
-                    <img className='about-img' src="/Optician_Marketing.jpg" alt="no image found" />
-                </div>
-                <div className='about-text'>
-                    <h1>בונים אתרים שמעצימים רעיונות</h1>
-                    <h3>צוות מוסמך של מתכנתים המתמחים בפיתוח רשת  שדואג להעניק לחלומות שלכם מקום ברשת העולמית!</h3>
-                    <h3>
-                        בין אם זה דף נחיתה, אתר אינטראקטיבי, מערכת מורכבת לניהול משתמשים או כלי אוטומציה מבוססי AI.
-                        לרשותנו צוות מתכנתים מומחה בעולם פיתוח הרשת
-                        שיודע לענות על כל הדרישות שלכם</h3>
-                    <h2>שילווה אתכם ויספק מענה לאורך כל הדרך</h2>
-                </div>
+
+        <div className='about-body'>
+            <div className='about-image-container'>
+                <img className='about-img' src="/Optician_Marketing.jpg" alt="no image found" />
+            </div>
+            <div className='about-text'>
+                <h1>בונים אתרים שמעצימים רעיונות</h1>
+                <h3>צוות מוסמך של מתכנתים המתמחים בפיתוח רשת  שדואג להעניק לחלומות שלכם מקום ברשת העולמית!</h3>
+                <h3>
+                    בין אם זה דף נחיתה, אתר אינטראקטיבי, מערכת מורכבת לניהול משתמשים או כלי אוטומציה מבוססי AI.
+                    לרשותנו צוות מתכנתים מומחה בעולם פיתוח הרשת
+                    שיודע לענות על כל הדרישות שלכם</h3>
+                <h2>שילווה אתכם ויספק מענה לאורך כל הדרך</h2>
             </div>
         </div>
+
     );
 }
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -9,14 +9,7 @@ import { motion } from "framer-motion";
 const HomePage = () => {
     return (
 
-        // <div className='hero-video-container'>
-        //     <video autoPlay muted loop playsInline className='hero-video' >
-        //         <source src='archweb_hero_video.webm' type='video/webm' />
-        //     </video>
-        //     <div className='hero-text'>
-        //         <h1>Archweb</h1>
-        //     </div>
-        // </div>
+
         <div className='hero-page'>
             <div className='hero-img-container'>
                 <img src='/archweb_logo.PNG' alt='archweb logo' className='hero-img' />
@@ -35,24 +28,6 @@ const HomePage = () => {
                     }}
                     animate={{
                         backgroundPosition: ["0% 50%", "100% 50%", "0% 50%"],
-                        // boxShadow: ["0 0 10px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 15px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 20px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 25px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 30px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 35px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 40px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 45px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 50px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 45px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 40px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 35px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 30px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 25px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 20px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 15px rgba(0, 195, 255, 0.9)",
-                        //     "0 0 10px rgba(0, 195, 255, 0.9) ",
-                        // ],
                     }}
                     transition={{
                         duration: 3,
